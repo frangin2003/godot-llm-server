62bb33023907c59286b50c928ead111202c9a644
Charles-Philippe Bernard <charlesphilippe.bernard@gmail.com>
2024-06-30 22:30:53 +0100

    LLM server ready

--- run.py
+++ run.py
@@ -22,56 +22,61 @@ global_speaker.Voice = global_speaker.GetVoices().Item(0)
 async def a_call_llm(websocket, data):
 
     prompt = llm_instance.get_prompt_from_messages(data)
-    command = ""
     all_chunks = ""
     buffer = ""
-    text = ""
-    capturing_response = False
-    capturing_command = False
+ 
+    capturing_speaker = False
+    speaker = ""
+    capturing_text = False
     at_least_one_chunk_has_been_sent = False
-
+    text = ""
+    capturing_command = False
+    command = ""
+ 
     await websocket.send("<|begin_of_text|>")
+    await asyncio.sleep(0)
     for chunk in llm_instance.llm._stream(prompt):
         chunk_text = chunk.text
-        # print(chunk_text)
-
+        print(chunk_text)
         # Clean and manage the buffer
         chunk_text = chunk_text.replace('","', '').replace(',"', '').replace('",', '').replace('"', '').replace('{', '').replace('}', '').replace(':', '').replace('=', '').replace('```', '')
         all_chunks += chunk_text
-
+ 
         buffer += chunk_text
         if (chunk_text.strip() == "_"
-            and buffer != "_text" and buffer != "_command"
-            and buffer != "_text_" and buffer != "_command_"):
+            and buffer != "_speaker" and buffer != "_text" and buffer != "_command"):
             buffer = "_"
             continue
 
-        if (buffer == "_text" or buffer == "_command"):
-            continue
-
-        # Check and transition based on keywords
-        if buffer == "_text_":
-            capturing_response = True
+        if buffer == "_speaker":
+            capturing_speaker = True
             buffer = ""  # Reset buffer after detecting keyword
             continue
-
-        elif buffer == "_command_":
-            capturing_response = False
+        elif buffer == "_text":
+            capturing_speaker = False
+            capturing_text = True
+            buffer = ""  # Reset buffer after detecting keyword
+            continue
+        elif buffer == "_command":
+            capturing_text = False
             capturing_command = True
             buffer = ""  # Reset buffer after detecting keyword
             continue
 
         # Accumulate content into the response or command based on the current state
-        if capturing_response:
+        if capturing_speaker:
+            speaker += chunk_text
+        elif capturing_text:
             at_least_one_chunk_has_been_sent = True
             text += chunk_text
-            print(chunk_text)
             await websocket.send(chunk_text)
+            await asyncio.sleep(0)
         elif capturing_command:
             command += chunk_text
 
     if not at_least_one_chunk_has_been_sent:
         await websocket.send(all_chunks)
+        await asyncio.sleep(0)
 
     print(all_chunks)
 
@@ -81,8 +86,10 @@ async def a_call_llm(websocket, data):
     print(f'FINAL command=|{command}|')
     if command:
         await websocket.send(f"<|command|>{command}")
+        await asyncio.sleep(0)
 
     await websocket.send("<|end_of_text|>")
+    await asyncio.sleep(0)
 
 async def websocket_handler(websocket, path):
     async for message in websocket:
