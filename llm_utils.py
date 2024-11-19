f7a618bdb1fa09b85f68ce5b673da88952353374
Charles-Philippe Bernard <charlesphilippe.bernard@gmail.com>
2024-11-19 19:30:48 +0000

    fixed typo

--- llm_utils.py
+++ llm_utils.py
@@ -4,18 +4,18 @@ from llm.Phi3ChatMLLlm import Phi3ChatMLLlm
 from llm.MistralChatMLLlm import MistralChatMLLlm
 
 def create_llm_instance(model_name, debug=False):
-    llm_intance = None
+    llm_instance = None
     if model_name in ["gpt4", "gpt4-turbo", "gpt4o"]:
-        llm_intance = OpenAILlm(model_name, debug)
+        llm_instance = OpenAILlm(model_name, debug)
     elif model_name.startswith("llama3"):
-        llm_intance = Llama3Llm(model_name, debug)
+        llm_instance = Llama3Llm(model_name, debug)
     elif model_name in ["phi3"]:
-        llm_intance = Phi3ChatMLLlm(model_name, debug)
+        llm_instance = Phi3ChatMLLlm(model_name, debug)
     elif model_name in ["mistral"]:
-        llm_intance = MistralChatMLLlm(model_name, debug)
+        llm_instance = MistralChatMLLlm(model_name, debug)
     else:
         raise ValueError(f"Unknown model name: {model_name}")
 
-    print("llm_intance and LLM initialized")
-    return llm_intance
+    print("llm_instance and LLM initialized")
+    return llm_instance
 
